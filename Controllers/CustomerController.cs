6987532a79374a3da42f9f3dcb95f70cd7a52d9c
zyjeng11 <zyjeng11@gmail.com>
2020-04-27 15:55:55 +0800

    Add save or edit if-else statement

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -53,9 +53,19 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if(customer.Id == 0)                            
+                 _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.MemberShipTypeId = customer.MemberShipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            }
+
             _context.SaveChanges();
             return RedirectToAction("Index", "Customer");
         }
