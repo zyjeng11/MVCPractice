276924bd5adb1bfcb490498d52cff73c9b69203d
zyjeng11 <zyjeng11@gmail.com>
2020-05-11 17:07:40 +0800

    add customer dto

---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Min18Member]
+        public DateTime? Birthday { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MemberShipTypeId { get; set; }
+    }
+}
